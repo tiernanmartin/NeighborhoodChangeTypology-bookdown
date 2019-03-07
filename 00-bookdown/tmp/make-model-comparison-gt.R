774cfda99bfe41f1376fb417ac334884c24434fb
Tiernan Martin <tiernan.martin@gmail.com>
2019-03-07 05:53:05 -0800

    add the second half of methods

--- 00-bookdown/tmp/make-model-comparison-gt.R
+++ 00-bookdown/tmp/make-model-comparison-gt.R
@@ -11,4 +11,6 @@ comp_tbl <- readr::read_csv("00-bookdown/data/model-comparison-table-20190306.cs
 
 # PLOT --------------------------------------------------------------------
 
-gt(comp_tbl)
+comp_tbl %>% 
+  gt() %>% 
+  gt::tab_style(gt::cells_styles(bkgd_color = '#FFFFFF'), gt::cells_data(gt::everything()))
