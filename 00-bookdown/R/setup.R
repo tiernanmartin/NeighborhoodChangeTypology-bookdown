294bb4241fae600591c866de6d4e61e8e9a3e0f7
Tiernan Martin <tiernan.martin@gmail.com>
2019-03-10 00:02:42 -0800

    add a faceted histogram chart for the 2013-2017 vulnerability indicators

--- 00-bookdown/R/setup.R
+++ 00-bookdown/R/setup.R
@@ -10,6 +10,9 @@ library(leaflet)
 library(showtext) 
 library(cowplot)
 library(magick)
+library(patchwork)
+library(glue)
+library(GGally)
 
 # FUNCTIONS ---------------------------------------------------------------
 
@@ -43,6 +46,13 @@ theme_nct <- theme_minimal(base_size = 10,
         plot.subtitle = element_text(hjust = 0),
         plot.caption = element_text(hjust = 0))
 
+theme_nct_8 <- theme_minimal(base_size = 8,
+                           base_family = "Open Sans",
+                           base_line_size = 0.5) +
+  theme(plot.title = element_text(hjust = 0),
+        plot.subtitle = element_text(hjust = 0),
+        plot.caption = element_text(hjust = 0))
+
 theme_void_nct <- theme_void(base_size = 10,
                            base_family = "Open Sans",
                            base_line_size = 1) +
@@ -61,6 +71,13 @@ colors_nct_typology <- c(
   "Late" = "#0794d0",
   "Continued Loss" = "#1b5f8f"
 )
+
+colors_nct_list <- list( "Susceptible" = "#ffff00",
+  "Early Type 1" = "#ffc800",
+  "Early Type 2" = "#ff9600",
+  "Dynamic" = "#a30382",
+  "Late" = "#0794d0",
+  "Continued Loss" = "#1b5f8f")
 
 scale_discrete_nct <- function(aesthetic, ...) {
   ggplot2:::manual_scale(
